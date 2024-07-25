e126e0063f64bfdf445e5ba5e924eb3fffb300d7
jhosuapp <jhosuapvll@gmail.com>
2024-07-24 22:29:50 -0500

    feat: useLayoutEeffect

--- src/03-examples/PokemonCard.tsx
+++ src/03-examples/PokemonCard.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useRef, useState } from "react"
 
 interface IPokemon {
     id?: string,
@@ -5,10 +6,33 @@ interface IPokemon {
     sprites?: string[]
 }
 
+interface IPosition {
+    width: number,
+    height: number
+}
+
 const PokemonCard = ({ name, id, sprites}:IPokemon):JSX.Element => {
+
+    const nodeSection = useRef<HTMLElement>(null);
+
+    const [position, setPosition] = useState<IPosition>({
+        width: 0,
+        height: 0
+    });
+
+    useLayoutEffect(()=>{
+        if(nodeSection.current){
+            const { width, height } = nodeSection.current?.getBoundingClientRect();
+            setPosition({
+                width,
+                height
+            });
+        }
+    },[]);
 
     return (
         <section 
+            ref={ nodeSection }
             style={{height: 200}}
         >
             <h2 className="text-capitalize">{ id } - { name }</h2>
@@ -20,6 +44,9 @@ const PokemonCard = ({ name, id, sprites}:IPokemon):JSX.Element => {
                     ))
                 }
             </div>  
+            <code>
+                { JSON.stringify( position ) }
+            </code>
         </section>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,11 @@ import ReactDOM from 'react-dom/client';
 // import { SimpleForm } from './02-use-effect/SimpleForm';
 // import { SimpleFormWithHook } from './02-use-effect/SimpleFormWithHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { FocusScreen } from './04-use-ref/FocusScreen';
+// import { FocusScreen } from './04-use-ref/FocusScreen';
+import { Layout } from './05-use-layout-effect/Layout';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-    <FocusScreen />
+    <Layout />
   // </React.StrictMode>,
 )
