142cd5d5033a5e0bca7d50e719de735b408c9463
jhosuapp <jhosuapvll@gmail.com>
2024-07-26 08:13:27 -0500

    feat: memo

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,11 @@ import ReactDOM from 'react-dom/client';
 // import { SimpleFormWithHook } from './02-use-effect/SimpleFormWithHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-use-ref/FocusScreen';
-import { Layout } from './05-use-layout-effect/Layout';
+// import { Layout } from './05-use-layout-effect/Layout';
+import { Memorize } from './06-memos/Memorize';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-    <Layout />
+    <Memorize />
   // </React.StrictMode>,
 )
