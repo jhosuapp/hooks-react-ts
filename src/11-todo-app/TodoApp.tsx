a01fc84ec882bf96cb5f0607319aa7fae357e27e
jhosuapp <jhosuapvll@gmail.com>
2024-08-21 20:25:47 -0500

    feat: ui todo's

--- src/11-todo-app/TodoApp.tsx
+++ src/11-todo-app/TodoApp.tsx
@@ -22,7 +22,38 @@ const TodoApp = ():JSX.Element => {
 
     return (
         <section className="container mt-5">
-            <h1>Esta es mi todo app</h1>
+            <h1>Todo app</h1>
+
+            <hr />
+
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group">
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                            <span>
+                                mi item
+                            </span>
+                            <button className="btn btn-danger">delete</button>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>add Todo</h4>
+                    <hr />
+                    <form>
+                        <input type="text" 
+                            placeholder=""
+                            className="form-control"
+                        />
+                        <button 
+                            type="submit"
+                            className="btn btn-outline-primary mt-1"
+                        >
+                            Add Todo
+                        </button>
+                    </form>
+                </div>
+            </div>
         </section>
     )
 }
