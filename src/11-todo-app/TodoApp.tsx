63017efb5ff6e06aa85dc4e4547c8c13dc38be76
jhosuapp <jhosuapvll@gmail.com>
2024-09-15 15:31:04 -0500

    feat: custom hook for todo

--- src/11-todo-app/TodoApp.tsx
+++ src/11-todo-app/TodoApp.tsx
@@ -1,47 +1,10 @@
-import { useEffect, useReducer } from "react"
-import { IinitialTodos, IinitialTodo, IActionTodoReducer } from './models/index';
-import { todoReducer } from "./reducers/index";
 import { TodoList, TodoAdd } from './components/index';
+import { useTodo } from '../hooks';
 
-
-const init = ()=> {
-    const storedTodo = localStorage.getItem('Todo');
-    return storedTodo ? JSON.parse(storedTodo) : [];
-}
 
 const TodoApp = ():JSX.Element => {
 
-    const initialTodos:IinitialTodos = [];
-
-    const [ state, dispatch ] = useReducer(todoReducer, initialTodos, init);
-    //Save in local storage
-    useEffect(()=>{
-        localStorage.setItem('Todo', JSON.stringify(state));
-    }, [ state ]);
-    //update state
-    const handleTodo = (Todo:IinitialTodo) =>{
-        const action:IActionTodoReducer = {
-            type: '[TODO] Add todo',
-            payload: Todo
-        }
-        dispatch(action);
-    }
-    //Delete todo
-    const handleTodoDelete = (Todo:IinitialTodo) =>{
-        const action:IActionTodoReducer = {
-            type: '[TODO] Delete todo',
-            payload: Todo  
-        }
-        dispatch(action);
-    }
-    //Check and uncheck todo
-    const handleTodoToggle = (Todo:IinitialTodo) =>{
-        const action:IActionTodoReducer = {
-            type: '[TODO] Toggle todo',
-            payload: Todo  
-        }
-        dispatch(action);
-    }
+    const { state, handleTodoDelete, handleTodoToggle, handleTodo } = useTodo();
 
     return (
         <section className="container mt-5">
