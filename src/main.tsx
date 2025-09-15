5e1c36302dd4692dc46a0919146538db5b227373
jhosuapp <jhosuapvll@gmail.com>
2025-09-15 11:33:45 -0500

    feat: useOptimistic

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'; 
 import ReactDOM from 'react-dom/client';
-import { TodoApp } from './11-todo-app/TodoApp';
+// import { TodoApp } from './11-todo-app/TodoApp';
 // import { App } from './App.tsx';
 // import { Counter } from './01-use-state/Counter';
 // import { CounterWithHook } from './01-use-state/CounterWithHook';
@@ -12,10 +12,11 @@ import { TodoApp } from './11-todo-app/TodoApp';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './07-use-memo/MemoHook';
 // import { CallbackHook } from './08-use-callback/CallbackHook';
+import { InstagramApp } from './12-use-optimistic/InstagramApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 //   // <React.StrictMode>
-    <TodoApp />
+    <InstagramApp />
 //   // </React.StrictMode>,
 )
